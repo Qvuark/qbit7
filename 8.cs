22c92439069b986131aa574a150d669b8f2badfc
Qvuark <roma18902015@gmail.com>
2024-04-06 01:50:30 +0300

    eighth excersise

---
+++ 8.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using System.Xml.Serialization;
+using System.Linq;
+
+class qbit
+{
+    static void Main()
+    {
+        int rows = int.Parse(Console.ReadLine());
+        int[,] array = new int[rows,rows];
+        FillArraySpiral(ref array);
+        PrintArray(array);
+        Console.ReadKey();
+    }
+    static void FillArraySpiral(ref int[,] array)
+    {
+        int value = 1;
+        int rowStart = 0, rowEnd = array.GetLength(0) - 1;
+        int colStart = 0, colEnd = array.GetLength(1) - 1;
+
+        while (rowStart <= rowEnd && colStart <= colEnd)
+        {
+            // Fill top row
+            for (int i = colStart; i <= colEnd; i++)
+            {
+                array[rowStart, i] = value++;
+            }
+            rowStart++;
+
+            // Fill right column
+            for (int i = rowStart; i <= rowEnd; i++)
+            {
+                array[i, colEnd] = value++;
+            }
+            colEnd--;
+
+            // Fill bottom row (if applicable)
+            if (rowStart <= rowEnd)
+            {
+                for (int i = colEnd; i >= colStart; i--)
+                {
+                    array[rowEnd, i] = value++;
+                }
+                rowEnd--;
+            }
+
+            // Fill left column (if applicable)
+            if (colStart <= colEnd)
+            {
+                for (int i = rowEnd; i >= rowStart; i--)
+                {
+                    array[i, colStart] = value++;
+                }
+                colStart++;
+            }
+        }
+    }
+
+    static void PrintArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- obj/Debug/net8.0/qbit7.AssemblyInfo.cs
+++ obj/Debug/net8.0/qbit7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("qbit7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a05ecbad5b2f2eceb122d62970670836f5cb9207")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1741460da611424d15720627f624c6cda8c9b344")]
 [assembly: System.Reflection.AssemblyProductAttribute("qbit7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("qbit7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
