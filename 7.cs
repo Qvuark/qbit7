1741460da611424d15720627f624c6cda8c9b344
Qvuark <roma18902015@gmail.com>
2024-04-06 01:40:25 +0300

    seventh excersise

---
+++ 7.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Xml.Serialization;
+using System.Linq;
+
+class qbit
+{
+    static void Main()
+    {
+        int[] RowsAndCols = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[,] array = new int[RowsAndCols[0], RowsAndCols[1]];
+        for (int i = 0; i < RowsAndCols[0]; i++)
+        {
+            string[] temparray = Console.ReadLine().Split();
+            for (int j = 0; j < RowsAndCols[1]; j++)
+            {
+                array[i, j] = int.Parse(temparray[j]);
+            }
+        }
+        Avarage(array);
+        Console.ReadKey();
+    }
+    static void Avarage(int[,] array)
+    {
+        double sum = 0;
+        int amount = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (i > j)
+                {
+                    sum += array[i,j];
+                    amount++;
+                }
+            }
+        }
+        Console.WriteLine(sum/amount);
+    }
+}
--- obj/Debug/net8.0/qbit7.AssemblyInfo.cs
+++ obj/Debug/net8.0/qbit7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("qbit7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36cf7b703ec5e5e5beb89787b75618c64d974318")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a05ecbad5b2f2eceb122d62970670836f5cb9207")]
 [assembly: System.Reflection.AssemblyProductAttribute("qbit7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("qbit7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
