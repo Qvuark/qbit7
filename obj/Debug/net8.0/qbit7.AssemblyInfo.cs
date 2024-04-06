2c31efb14306681ebf8c727cd399d920dcd1b8dc
Qvuark <roma18902015@gmail.com>
2024-04-06 16:36:35 +0300

    tenth excersise

--- obj/Debug/net8.0/qbit7.AssemblyInfo.cs
+++ obj/Debug/net8.0/qbit7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("qbit7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c92439069b986131aa574a150d669b8f2badfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11b3158ace58f300f9e3b0693e6ced5a44b540cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("qbit7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("qbit7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
